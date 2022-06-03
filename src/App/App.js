6b5c3c7471a0585fd5531d2c6ecef9e7dedb88a0
Samuel Uamba <lelifork@gmail.com>
2022-06-03 09:04:34 +0200

    add

--- src/App/App.js
+++ src/App/App.js
@@ -8,35 +8,11 @@ import {
 } from "@material-ui/core";
 
 import { Routes, Route, HashRouter } from "react-router-dom";
-import NavegatorBars from "../components/sidebarDropDown/NavegatorBars";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#333996",
-      light: "#3c44b126",
-    },
-    secondary: {
-      main: "#f83245",
-      light: "#f8324526",
-    },
-    background: {
-      default: "#f4f5fd",
-    },
-  },
-  overrides: {
-    MuiAppBar: {
-      root: {
-        transform: "translateZ(0)",
-      },
-    },
-  },
-  props: {
-    MuiIconButton: {
-      disableRipple: true,
-    },
-  },
-});
+import Header from "../Header";
+import Menu from "../Menu";
+import AdminDashboard from "../AdminDashboard";
+import Footer from "../Footer";
 
 const useStyles = makeStyles({
   appMain: {
@@ -46,43 +22,13 @@ const useStyles = makeStyles({
 });
 
 function App() {
-  const classes = useStyles();
   return (
-    <ThemeProvider theme={theme}>
-      <div className={classes.appMain}>
-        <>
-          <HashRouter>
-            <NavegatorBars />
-            <Routes>
-              {/* <Route exact path="/" element={<Login />} />
-              <Route
-                exact
-                path="/DashboardCrentes"
-                element={<DashboardCrentes />}
-              />
-              <Route
-                exact
-                path="/DashboardFinancas"
-                element={<DashboardFinancas />}
-              />
-              <Route
-                exact
-                path="/DashboardAdmin"
-                element={<DashboardAdmin />}
-              />
-              <Route exact path="/addCrente" element={<Crentes />} />
-              <Route exact path="/addPresencas" element={<Presencas />} />
-              <Route exact path="/financas" element={<Financas />} />
-              <Route exact path="/liquidar" element={<Liquidar />} />
-              <Route exact path="/ajuda" element={<Employees />} /> */}
-            </Routes>
-            {/* <Footer /> */}
-          </HashRouter>
-        </>
-      </div>
-
-      <CssBaseline />
-    </ThemeProvider>
+    <div class="wrapper">
+      <Header />
+      <Menu />
+      <AdminDashboard />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        {/* Main Footer */}
+        <footer className="main-footer">
+          <strong>Copyright © 2022</strong>
+          Todos Direitos Reservados.
+          <div className="float-right d-none d-sm-inline-block">
+            <b>Versão</b> 1.0
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
