998ba7aed910dd508d20e7fda454d979bea2a03d
amadeujoaquim <amadeuyoung@gmail.com>
2022-06-25 01:54:52 +0200

    grafico de agregados circular feito

---
+++ src/dashboards/PieChart.js
@@ -0,0 +1,61 @@
+import React, { PureComponent } from "react";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+
+const COLORS = ["#00c0ef", "#f39c12", "#f56954", "#00a65a"];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+export default class Example extends PureComponent {
+  render() {
+    const data = [
+      { name: "Group A", value: this.props.totalRequerimento },
+      { name: "Group B", value: this.props.totalNotasEntrada },
+      { name: "Group C", value: this.props.totalNotasSaidas },
+      { name: "Group D", value: this.props.totalAudiencias },
+    ];
+
+    return (
+      <PieChart width={300} height={200}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={100}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    );
+  }
+}
